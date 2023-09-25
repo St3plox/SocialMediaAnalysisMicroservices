e57cb72593a318b44a8a722daafc5f316475243d
JegorSt3p <st3pegor@gmail.com>
2023-09-25 21:14:09 +0700

    inter service communication

--- AnalysisService/src/main/java/com/tvey/DataAnalysisService/controller/AnalysisController.java
+++ AnalysisService/src/main/java/com/tvey/DataAnalysisService/controller/AnalysisController.java
@@ -1,9 +1,24 @@
 package com.tvey.DataAnalysisService.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tvey.DataAnalysisService.dto.ApiTransferObject;
+import com.tvey.DataAnalysisService.service.DataProcessor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/analysis")
 public class AnalysisController {
+
+    private final DataProcessor dataProcessor;
+
+    @GetMapping("/yt")
+    @ResponseStatus(HttpStatus.OK)
+    public List<? extends ApiTransferObject> getVideoComments(@RequestParam String url) {
+        return dataProcessor.tokenizeComments(url);
+    }
 }
--- AnalysisService/src/main/java/com/tvey/DataAnalysisService/service/DataProcessorImpl.java
+++ AnalysisService/src/main/java/com/tvey/DataAnalysisService/service/DataProcessorImpl.java
@@ -1,12 +1,26 @@
 package com.tvey.DataAnalysisService.service;
 
 import com.tvey.DataAnalysisService.dto.ApiTransferObject;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
-public class DataProcessorImpl implements DataProcessor{
+@RequiredArgsConstructor
+@Service
+public class DataProcessorImpl implements DataProcessor {
+
+    private final WebClient webClient;
+
     @Override
     public List<? extends ApiTransferObject> tokenizeComments(String url) {
-        return null;
+
+        return webClient.get()
+                .uri("http://localhost:8081/api/data/" +
+                        "yt?url=" + url)
+                .retrieve()
+                .bodyToMono(List.class)
+                .block();
     }
 }
