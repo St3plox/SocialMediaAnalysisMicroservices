0d87afd9ca3009c13c28873864755eaa477eaf77
st3pegor <st3pegor@gmail.com>
2023-10-16 21:43:18 +0700

    added UrlController for Data collection service

--- data-collection-service/src/main/java/ru/tveu/DataCollectionService/controller/UrlController.java
+++ data-collection-service/src/main/java/ru/tveu/DataCollectionService/controller/UrlController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.tveu.DataCollectionService.service.url.UrlProcessor;
 
@@ -15,7 +16,7 @@ public class UrlController {
     private final UrlProcessor urlProcessor;
 
     @GetMapping("/yt")
-    public ResponseEntity<String> processYtUrl(@PathVariable String url) {
+    public ResponseEntity<String> processYtUrl(@RequestParam String url) {
         return new ResponseEntity<>(urlProcessor.extractContentId(url), HttpStatus.OK);
     }
 }
